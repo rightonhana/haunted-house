a1f2fecf930d5c828e49f0752558b6d80fb71609
Florencia Gonzalez <rightonhana@gmail.com>
2021-07-12 14:03:33 -0700

    :hammer: add more graves

--- src/modules/graves.ts
+++ src/modules/graves.ts
@@ -5,9 +5,9 @@ export const graves = new Group();
 const graveGeometry = new BoxGeometry(0.6,0.8,0.2);
 const graveMaterial = new MeshStandardMaterial({color:"#b2b6b1"});
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 60; i++) {
 	const angle = Math.random() * Math.PI * 2;
-	const radiusX = 3 + Math.random() * 7;
+	const radiusX = 3 + Math.random() * 10;
 	const radiusZ = 3 + Math.random() * 2.8;
 	const x = Math.sin(angle) * radiusX;
 	const z = Math.cos(angle) * radiusZ;
