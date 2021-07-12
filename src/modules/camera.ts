87c7071fa59fb1b1f46faff40f36dfdb32306c8f
Florencia Gonzalez <rightonhana@gmail.com>
2021-07-12 10:26:00 -0700

    :sparkles: add camera

---
+++ src/modules/camera.ts
@@ -0,0 +1,7 @@
+import { PerspectiveCamera } from 'three';
+
+export const perspectiveCamera = (width: number, height: number) => {
+	const camera = new PerspectiveCamera(75, width / height, 0.1, 100);
+	camera.position.set(0, 2.8, 8.2);
+	return camera;
+}
